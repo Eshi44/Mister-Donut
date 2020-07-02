c96ab959d32c433c23854e6b34bbe76845dd8037
Eshi44 <e.hameliervolino@gmail.com>
2020-07-01 20:24:23 -0400

    installed npm packages and created package jsons

---
+++ server.js
@@ -0,0 +1,41 @@
+require("dotenv").config();
+const express = require("express");
+const path = require("path");
+// const mongoose = require("mongoose");
+
+// PORT
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+//MIDDLEWARE
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get("/api/config", (req, res) => {
+	res.json({
+		success: true,
+		currentPort: PORT,
+	});
+});
+
+//ROUTES
+
+// // MONGOOSE CONNECTION
+// mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/", {
+// 	useNewUrlParser: true,
+// 	useUnifiedTopology: true,
+// });
+
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static("client/build"));
+} else {
+	app.use(express.static("public"));
+}
+
+app.get("*", (req, res) => {
+	res.sendFile(path.join(__dirname, "/client/build/index.html"));
+});
+// PORT
+app.listen(PORT, () => {
+	console.log(`App is running on http://localhost:${PORT}`);
+});
